3a8e0cac8c70227c5d539722c5bcaf1fd232dfae
FamiliaSalinasVentura <86278046+FamiliaSalinasVentura@users.noreply.github.com>
2021-06-22 22:55:25 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+/*const miTitulo = document.querySelector('h1');
+miTitulo.textContent = '¡My Family!';*/
+
+let miImage = document.querySelector('img');
+miImage.onclick = function () {
+    let miSrc = miImage.getAttribute('src');
+    if (miSrc === 'images/bici1.jpg') {
+      miImage.setAttribute('src','images/mi-gran-familia.png');
+    } else {
+      miImage.setAttribute('src', 'images/bici1.jpg');
+    }
+}
+
+let miBoton = document.querySelector('button');
+let miTitulo = document.querySelector( 'h1');
+
+function estableceNombreUsuario() {
+    let miNombre = prompt('Introduzca su nombre.');
+    if(!miNombre) {
+        estableceNombreUsuario();
+    } else {
+        localStorage.setItem('nombre', miNombre);
+        miTitulo.innerHTML = 'Mozilla is genial, ' + miNombre;
+  }
+}
+
+if (!localStorage.getItem('nombre')) {
+    estableceNombreUsuario();
+}
+else {
+    let nombreAlmacenado = localStorage.getItem('nombre');
+    miTitulo.textContent = 'Mozilla es genial, ' + nombreAlmacenado;
+}
+
+miBoton.onclick = function() {
+    estableceNombreUsuario();
+}
